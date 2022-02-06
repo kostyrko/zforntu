5090d6b3ca765e088ea2a5926351868879782fdc
Mikołaj Kostyrko <mkostyrko@gmail.com>
2022-02-06 15:00:23 +0100

    Update content 06022022 - cypress/qa articles

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  // console.log('loaded');
   const closeBtn = document.querySelector('.aside--close')
   const openBtn = document.querySelector('.aside--open')
 
@@ -6,12 +7,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const main = document.querySelector('main')
   const aside = document.querySelector('aside')
 
+  const asideLS = localStorage.getItem('aside')
+
+  // if (asideLS==='closed') {
+  //   aside.classList.add("d--none");
+  //   main.classList.remove("main--margin")
+  // }
+
   const closeAside = (e) => {
     e.preventDefault()
     aside.classList.add("d--none");
     main.classList.remove("main--margin")
     container.classList.remove("container--smaller")
-
+    // localStorage.setItem('aside', 'closed')
   }
 
   const openAside = (e) => {
@@ -19,6 +27,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     aside.classList.remove("d--none");
     main.classList.add("main--margin")
     container.classList.add("container--smaller")
+    // localStorage.setItem('aside', 'open')
   }
 
   closeBtn.addEventListener('click', closeAside)
